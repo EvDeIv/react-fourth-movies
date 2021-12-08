5c5560b6e1f8dd974e28b578e491869e0039bc13
Yevhenii Deina <deinak1996@gmail.com>
2021-12-08 16:18:56 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default App;
